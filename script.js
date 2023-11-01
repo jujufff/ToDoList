1e145183c7866eb7807112fba9adb284ec3a8162
jujufff <149205402+jujufff@users.noreply.github.com>
2023-11-01 18:02:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,119 +1,127 @@
+const prompt = require('prompt-sync')();
 class tarefa {
-  constructor (Tarefa) {
-    this.tarefa = Tarefa
+  constructor(descricao, concluida ) {
+    this._tarefa = descricao;
+    this._concluida = concluida;
+  }
+
+  info(){
+    return ("Tarefa: " + this._tarefa + "\nEstado: " + this._concluida)
+
+
+  }
+
+}
+
+class tarefaRepetitiva extends tarefa {
+  constructor(descricao,concluida,frequencia) {
+    super(descricao,concluida)
+    this._frequencia = frequencia
+  }
+  info(){
+    return("\nTarefa: " + this._tarefa +"\nEstado: "+ this._concluida + "\nFrequência: " + this._frequencia+"\n")
   }
 }
 
+class tarefaPrioritaria extends tarefa {
+  constructor(descricao,concluida,nivelDePrioridade, dataLimite) {
+    super(descricao,concluida)
+    this.nivelDePrioridade = nivelDePrioridade
+    this.dataLimite = dataLimite
+  }
+  info(){
+    return("\nTarefa: " + this._tarefa +"\nEstado: " + this._concluida + "\n nivel de prioridade: " + this.nivelDePrioridade+" \ndata de limite: " + this.dataLimite+"\n")
+  }
+}
+
+class TarefaComEtiqueta extends tarefa {
+  constructor (descricao,concluida,etiquetas = []) {
+    super(descricao,concluida)
+    this.etiquetas 
+  }
+ info(){
+  return("Tarefa: "+this._tarefa+"\nEstado: "  + this._concluida + this.etiqueta)
+ }
+}
+
+//let tarefa1 = new tarefa(descricao, etiq)
+
+
 class toDoList {
-  constructor () {
+  constructor() {
     this.lista_tarefas = [];
   }
 
-  adicionarTarefa (tarefa) {
-    this.lista_tarefas.push({descricao: tarefa, concluida: false});
-  } 
-  
-  tarefaConcluida (posição) {
-    this.lista_tarefas[posição].concluida = true; 
+  adicionarTarefa(tarefa) {
+    this.lista_tarefas.push(tarefa);
   }
-  
-  removerTarefa (pos) {
-    this.lista_tarefas.splice (pos,1)
-  } 
 
-  exibirLista () {
-    console.log ("Tarefas:");
+  tarefaConcluida() {
     for (let i = 0; i < this.lista_tarefas.length; i++) {
-      let tarefa = this.lista_tarefas[i];
-      console.log(`${i + 1}. ${tarefa.descricao}`);
+      console.log(`${i}  -  ${this.lista_tarefas[i]._tarefa}`)
+     
+    }
+    let op = prompt("Digite o indice para concluir: ");
+    this.lista_tarefas[op]._concluida = true;
+  }
+
+  removerTarefa() {
+    for (let i = 0; i < this.lista_tarefas.length; i++) {
+      console.log(`${i}  -  ${this.lista_tarefas[i]._tarefa}`)
+     
+    }
+    let op = prompt("Digite o indice para remover: ");
+    this.lista_tarefas.splice(op, 1)
+  }
+
+  exibirLista() {
+    //console.log ("Tarefas:");
+    for (let i = 0; i < this.lista_tarefas.length; i++) {
+      console.log(this.lista_tarefas[i].info())
+      //let tarefa = this.lista_tarefas[i];
+      //console.log(`${i + 1}. ${tarefa.descricao}`);
+
     }
   }
-} 
-let tarefa1 = new tarefa("andar")
-let tarefa2 = new tarefa("banhar")
+}
+//const nome = document.getElementById("tarefaAdicionada").value
+let tarefa1 = new tarefa("comer",false)
+let tarefa2 = new tarefaPrioritaria("banhar",false,"alto","1452")
+let tarefa3 = new tarefaRepetitiva ("dormir",false,"todo dia")
 
 let listaTarefas = new toDoList()
 
-listaTarefas.adicionarTarefa(tarefa1)
-listaTarefas.adicionarTarefa(tarefa2)
 
-listaTarefas.exibirLista()
-
-
-
-
-class tarefaRepetitiva extends tarefa {
-   constructor () {
-     super()
+while(true){
+  let escolha = prompt("Voce tem as seguintes opcoes: \n\n1- adicionar tarefa\n2- concluir tarefa\n3- remover tarefa\n4- vizualizar tarefas\n\nDigite sua escolha:")
+ 
+   if(escolha == 1){
+        let decisao = prompt('1- tarefa normal\n2- tarefa prioritaria\n3- tarefa repetitiva\n4- tarefa com etiqueta')
+        if(decisao == 1){
+          let descricao = prompt("digite a descricao: ")
+          let tarefa1 = new tarefa(descricao,false)
+          listaTarefas.adicionarTarefa(tarefa1)
+         
+        }else if(decisao == 2){
+          let descricao = prompt("digite a descricao: ")
+          let prio = prompt("Digite o nivel de prioridade:")
+          let dtmax = prompt("Digite a data maxima")
+          let tarefa1 = new tarefaPrioritaria(descricao,false,prio,dtmax)
+          listaTarefas.adicionarTarefa(tarefa1)
+        }else if(decisao == 3){
+          let descricao = prompt("digite a descricao: ")
+          let frequencia = prompt("Digite a frequência:")
+          let tarefa1 = new tarefaRepetitiva(descricao,false,frequencia)
+          listaTarefas.adicionarTarefa(tarefa1)
+        }else if(decisao == 4){
+          let descricao = prompt("digite a descricao: ")
+          let etiqueta = prompt("Digite a etiqueta:")
+          let tarefa1 = new TarefaComEtiqueta(descricao,false,etiqueta)
+          listaTarefas.adicionarTarefa(tarefa1)
+        }
+        
+        else if(escolha == 'p'){
+    break;
    }
 }
-
-// class tarefaPrioritaria extends Tarefa {
-//   constructor () {
-//     super ()
-//   }
-// }
-
-// let tarefa = document.getElementById ("texto");
-
-
-
-
-/*class ListaDeTarefas {
-    constructor() {
-        this.tarefas = [];
-    }
-
-    adicionarTarefa(tarefa) {
-        this.tarefas.push({ nome: tarefa, concluida: false });
-        console.log(`Tarefa "${tarefa}" adicionada à lista.`);
-    }
-
-    removerTarefa(tarefa) {
-        const tarefaEncontrada = this.encontrarTarefa(tarefa);
-        if (tarefaEncontrada) {
-            this.tarefas = this.tarefas.filter(item => item !== tarefaEncontrada);
-            console.log(`Tarefa "${tarefa}" removida da lista.`);
-        } else {
-            console.log(`Tarefa "${tarefa}" não encontrada na lista.`);
-        }
-    }
-
-    marcarComoConcluida(tarefa) {
-        const tarefaEncontrada = this.encontrarTarefa(tarefa);
-        if (tarefaEncontrada) {
-            tarefaEncontrada.concluida = true;
-            console.log(`Tarefa "${tarefa}" marcada como concluída.`);
-        } else {
-            console.log(`Tarefa "${tarefa}" não encontrada na lista.`);
-        }
-    }
-
-    encontrarTarefa(tarefaNome) {
-        return this.tarefas.find(item => item.nome === tarefaNome);
-    }
-
-    exibirTarefas() {
-        console.log("Lista de Tarefas:");
-        this.tarefas.forEach((tarefa, index) => {
-            console.log(`${index + 1}. ${tarefa.nome} - ${tarefa.concluida ? 'Concluída' : 'Pendente'}`);
-        });
-    }
 }
-
-// Criar uma instância da classe ListaDeTarefas
-const minhaLista = new ListaDeTarefas();
-
-// Adicionar tarefas à lista
-minhaLista.adicionarTarefa("Fazer compras");
-minhaLista.adicionarTarefa("Estudar JavaScript");
-minhaLista.adicionarTarefa("Fazer exercícios");
-
-// Exibir tarefas
-minhaLista.exibirTarefas();
-
-// Marcar tarefa como concluída
-minhaLista.marcarComoConcluida("Estudar JavaScript");
-
-// Exibir tarefas após marcação
-minhaLista.exibirTarefas();*/
